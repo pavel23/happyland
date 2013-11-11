9b8ac932801ec1709c63a51027bcb85a0a87571b
Javier Roque <jroquee@gmail.com>
2013-11-10 20:22:19 -0500

    perfiles registra y elimina correctamente

--- system/application/models/ModuleDao.php
+++ system/application/models/ModuleDao.php
@@ -29,12 +29,13 @@ class ModuleDao extends CI_Model {
             $this->db->where_in('mdl.id', $a_module_ids);
         }
         $this->db->select('mdl.id, mpa.module_id, mdl.name, mdl.description, mdl.parent_id, mpa.read, mpa.write, mpa.download');
+        $this->db->from('hpl_module mdl');
+        $this->db->join('hpl_module_profile_access mpa', 'mdl.id=mpa.module_id AND mpa.profile_id='.$profile_id, 'left');
         $this->db->where('mdl.parent_id', NULL);
         $this->db->where('mdl.is_deleted', 0);
         $this->db->where('mdl.status', Status::STATUS_ACTIVO);
+        //$this->db->where('mpa.profile_id', $profile_id);
         $this->db->order_by('mdl.position_order', 'ASC');
-        $this->db->from('hpl_module mdl');
-        $this->db->join('hpl_module_profile_access mpa', 'mdl.id=mpa.module_id', 'left');
         $query = $this->db->get();
         return $query->result();
     }
