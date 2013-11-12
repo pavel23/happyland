728994e7c0543486ca86ba1bcf98ca45c45b287a
Javier Roque <jroquee@gmail.com>
2013-11-11 20:37:44 -0500

    table ajax

--- assets/js/happy/profile.modal.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    //console.log($('input=checkbox .parent_read'));
-    $('input[type=checkbox] .parent_read').on( 'click', function(){
-      console.log($(this));  
-    });
-    
-});
