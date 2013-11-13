c47e6a5de2a635d7aef3507d538be2da055d0b64
jreboss <jroquee@gmail.com>
2013-11-13 18:13:14 -0500

    subiendo cambios pendientes

---
+++ system/application/views/Profile/testTable.php
@@ -0,0 +1,23 @@
+ <div class="row">
+    <div class="col-lg-12">
+        <div class="box">
+            <header>
+                <div class="icons"><i class="icon-th"></i></div>
+                <h5>Listado de Perfiles</h5>
+            </header>
+            <div class="body">
+                <table id="example" class="table table-bordered table-condensed table-hover table-striped">
+                     <thead>
+                         <tr>
+                             <th>Cabecera 1</th>
+                             <th>Cabecera 2</th>
+                             <th>Cabecera 3</th>
+                             <th>Cabecera 4</th>
+                             <th>Cabecera 5</th>
+                         </tr>
+                     </thead>
+                 </table>         
+            </div>
+        </div>
+    </div>
+ </div>
